3662264cb6ce3fa4733aa2a8c6b6dc449e0ebb33
Mark Lawson <mlawson3691@gmail.com>
2016-09-23 08:33:57 -0700

    write a test for stylist save method

---
+++ tests/StylistTest.php
@@ -0,0 +1,19 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Stylist.php";
+
+    $server = 'mysql:host=localhost;dbname=hair_salon_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class StylistTest extends PHPUnit_Framework_TestCase
+    {
+        
+    }
+
+?>
