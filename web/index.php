d7dd6ad2df573cc9593e997228ce777d3a2f8f14
Mark Lawson <mlawson3691@gmail.com>
2016-09-23 10:13:17 -0700

    link bootstrap in head of index.php

--- web/index.php
+++ web/index.php
@@ -2,3 +2,13 @@
     $website = require_once __DIR__.'/../app/app.php';
     $website->run();
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link href='/assets/css/bootstrap.min.css' rel='stylesheet' type='text/css'>
+    <link href='/assets/css/styles.css' rel='stylesheet' type='text/css'>
+    <title>Hair Salon</title>
+</head>
+</html>
