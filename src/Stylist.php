273ed87097bfe80c654978f8b92c98e8c441dc7f
Mark Lawson <mlawson3691@gmail.com>
2016-09-23 08:29:08 -0700

    create Stylist class with construcor, getters, and setters

---
+++ src/Stylist.php
@@ -0,0 +1,33 @@
+<?php
+    class Stylist
+    {
+        private $name;
+        private $id;
+
+        function __construct($name, $id = null)
+        {
+            $this->name = $name;
+            $this->id = $id;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function setName($new_name)
+        {
+            $this->name = $new_name;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function setId($new_id)
+        {
+            $this->id = $new_id;
+        }
+    }
+?>
