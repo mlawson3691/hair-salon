6c160f62192a6df5e0de23934bd043b6b1006870
Mark Lawson <mlawson3691@gmail.com>
2016-09-23 10:47:13 -0700

    create Client class with constructor, getters, and setters

---
+++ src/Client.php
@@ -0,0 +1,74 @@
+<?php
+    class Client
+    {
+        private $name;
+        private $id;
+
+        function __construct($name, $stylist_id, $id = null)
+        {
+            $this->name = $name;
+            $this->stylist_id = $stylist_id;
+            $this->id = $id;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function setName($new_name)
+        {
+            $this->name = $new_name;
+        }
+
+        function getStylistId()
+        {
+            return $this->stylist_id;
+        }
+
+        function setStylistId($new_stylist_id)
+        {
+            $this->stylist_id = $new_stylist_id;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function setId($new_id)
+        {
+            $this->id = $new_id;
+        }
+
+        function save()
+        {
+
+        }
+
+        static function getAll()
+        {
+
+        }
+
+        static function deleteAll()
+        {
+
+        }
+
+        static function find($search_id)
+        {
+
+        }
+
+        function update($new_name)
+        {
+
+        }
+
+        function delete()
+        {
+
+        }
+    }
+?>
