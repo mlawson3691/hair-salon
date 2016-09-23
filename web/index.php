67d434ef6dcae7a8104aea13d5456ba765dbdfcb
Mark Lawson <mlawson3691@gmail.com>
2016-09-23 13:14:46 -0700

    link font

--- web/index.php
+++ web/index.php
@@ -9,6 +9,7 @@
     <meta charset="utf-8">
     <link href='/assets/css/bootstrap.min.css' rel='stylesheet' type='text/css'>
     <link href='/assets/css/styles.css' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
     <title>Hair Salon</title>
 </head>
 </html>
